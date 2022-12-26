8c297f1b19a34dd9c8704cf29b05c10f694fbb9d
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2022-12-26 16:21:44 -0300

    @RafaelCandeias00 [Task: add componente menu]

--- categorias/src/app/app.module.ts
+++ categorias/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { CategoriasDetalheComponent } from './categorias-detalhe/categorias-detalhe.component';
 import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
 import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {  
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule } from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+import { LayoutModule } from '@angular/cdk/layout';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +35,27 @@ import { LogoutComponent } from './logout/logout.component';
     CategoriaNovaComponent,
     CategoriaEditarComponent,
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,  
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule, 
+    MatSelectModule,
+    MatSidenavModule,  
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
