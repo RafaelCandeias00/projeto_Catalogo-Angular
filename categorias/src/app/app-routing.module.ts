323391ee2d16e7db3b8c101ae387d33d8df404cb
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2022-12-26 15:49:04 -0300

    @RafaelCandeias00 [Task: add configuração de rotas]

--- categorias/src/app/app-routing.module.ts
+++ categorias/src/app/app-routing.module.ts
@@ -1,7 +1,62 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriasDetalheComponent } from './categorias-detalhe/categorias-detalhe.component';
+import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login'
+    }
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    data: {
+      title: 'Logout'
+    }
+  },
+  {
+    path: 'categorias',
+    component: CategoriasComponent,
+    data:{
+      title: 'Lista de Categorias'
+    }
+  },
+  {
+    path: 'categoria-detalhe/:id',
+    component: CategoriasDetalheComponent,
+    data:{
+      title: 'Lista de Categorias'
+    }
+  },
+  {
+    path: 'categoria-nova',
+    component: CategoriaNovaComponent,
+    data:{
+      title: 'Adicionar Categorias'
+    }
+  },
+  {
+    path: 'categoria-editar/:id',
+    component: CategoriaEditarComponent,
+    data:{
+      title: 'Editar a Categoria'
+    }
+  },
+  {
+    path: '',
+    redirectTo: '/categorias',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
