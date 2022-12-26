64678385ba9cf20910bcb69cd3c7710e09e57665
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2022-12-26 14:57:36 -0300

    @RafaelCandeias00 [Task: add componentes]

---
+++ categorias/src/app/categorias-detalhe/categorias-detalhe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-detalhe',
+  templateUrl: './categorias-detalhe.component.html',
+  styleUrls: ['./categorias-detalhe.component.scss']
+})
+export class CategoriasDetalheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
